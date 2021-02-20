cba78e1e1c8cf5a10ad15339d9c9bb6874730fd1
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-02-20 19:02:59 +0000

    login user type

--- api/src/graphql/schemas/types.ts
+++ api/src/graphql/schemas/types.ts
@@ -1,7 +1,11 @@
 export const types = `
 type User {
   _id: ID!
-  firstName: String!
-  lastName: String!
+  username: String
   email: String
-}`;
+}
+type LoggedInUser {
+  token: String
+  user: User
+}
+`;
