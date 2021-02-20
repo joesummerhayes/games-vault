fad431976b2780cf0f385a4075a420aaa91beb29
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-02-20 19:03:48 +0000

    redux setup

--- app-client/src/index.tsx
+++ app-client/src/index.tsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  Store,
+} from 'redux';
+import reduxThunk from 'redux-thunk';
+import { batchedSubscribe } from 'redux-batched-subscribe';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import reducer from './reducers';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+import { ActionTypes } from './actions/types';
+// import { muiTheme } from './theme';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store: Store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(reduxThunk),
+    composeEnhancers(),
+    batchedSubscribe((notify) => {
+      notify();
+    }),
+  ),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: ActionTypes.loginUser });
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/* <MuiThemeProvider theme={muiTheme}> */}
+      <App />
+    {/* </MuiThemeProvider> */}
+  </Provider>,
+  document.getElementById('root'),
+);
+
+// serviceWorker.unregister();
