ed4e64c5529cf6836ca1e390bdb54c9a6df207df
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-02-20 19:01:49 +0000

    login resolver

--- api/src/graphql/resolvers/auth.ts
+++ api/src/graphql/resolvers/auth.ts
@@ -1,20 +1,24 @@
 import bcrypt from 'bcrypt';
 import { validateUser } from './utils';
 import User, { IUser } from '../modals/user';
+import jwt from 'jsonwebtoken';
 
 interface CreateUser {
-  email: string;
-  password: string;
-  username: string;
+  userInput: GVType.SignupUser
 }
 
 interface GetUser {
   email: string;
 }
 
+interface Login {
+  email: string;
+  password: string;
+}
+
 export default {
   async createUser(args:  CreateUser, _req: Request): Promise<IUser> {
-    const { email, username, password } = args;
+    const { userInput: { email, username, password } } = args;
     const existingUser = await User.findOne({ email })
     if (existingUser) {
       return existingUser;
@@ -30,7 +34,8 @@ export default {
     });
 
     try {
-      newUser.save();      
+      newUser.save();
+      return newUser;
     } catch(err) {
       console.log(err);
       throw err;
@@ -45,4 +50,27 @@ export default {
     }
     return user;
   },
+  async login(args: Login, _req: Request): Promise<GVType.LoggedInUser> {
+    const { email, password } = args;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('user not found');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('password is incorrect');
+      throw error;
+    }
+    const token = jwt.sign({
+      userId: user._id.toString(),
+      email: user.email,
+    }, 'supersecretsecret', { expiresIn: '1h' });
+    const parsedUser = {
+      _id: user._id.toString(),
+      email: user.email,
+      username: user.username
+    }
+
+    return { token, user: parsedUser };
+  }
 }
