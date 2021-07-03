06406d38cb0f91a96cdd05b0105bb3e473d9252f
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-07-03 16:43:33 +0100

    small code clean up

--- api/src/graphql/resolvers/utils.ts
+++ api/src/graphql/resolvers/utils.ts
@@ -6,24 +6,22 @@ interface ValidationError {
 
 export const validateUser = (email: string, password: string): void => {
   const errors: ValidationError[] = [];
-    if (!validator.isEmail(email)) {
-      errors.push({ message: 'Email is not valid' });
-    }
-    if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
-      errors.push({ message: 'password is too short' });
-    }
-    if (errors.length > 0) {
-
-      const reducer = (acc, cur) => {
-        if (cur.message) {
-          acc += cur.message;
-        }
-        return acc;
+  if (!validator.isEmail(email)) {
+    errors.push({ message: 'Email is not valid' });
+  }
+  if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+    errors.push({ message: 'password is too short' });
+  }
+  if (errors.length > 0) {
+    const reducer = (acc, cur) => {
+      if (cur.message) {
+        acc += cur.message;
       }
+      return acc;
+    };
 
-      const errorString = errors.map((error) => error.message).reduce(reducer);
-      const error = new Error(errorString);
-      throw error;
-    }
-    return;
-}
+    const errorString = errors.map((error) => error.message).reduce(reducer);
+    const error = new Error(errorString);
+    throw error;
+  }
+};
