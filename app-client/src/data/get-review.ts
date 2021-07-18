1390ec6f1fd5dd1258cd2b675e96458fdff48039
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-07-18 17:25:49 +0100

    get-review graphql query

---
+++ app-client/src/data/get-review.ts
@@ -0,0 +1,29 @@
+import graphQL from './graph-ql';
+
+interface ReviewQLResult {
+  review: GVType.Review;
+}
+
+interface IGetReview {
+  id: string;
+}
+
+const getReview = async (variables: IGetReview): Promise<GVType.Review> => {
+  const response = await graphQL.query<ReviewQLResult, IGetReview>(`
+    query getReview($id: String!){
+      review(id: $id) {
+        console
+        images
+        rating
+        synopsis
+        title
+        userId
+      }
+    }`, variables);
+  if (response === null) {
+    throw new Error('could not get review details');
+  }
+  return response.review;
+};
+
+export default getReview;
