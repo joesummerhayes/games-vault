7c24e6896cbfe7edc9683fac76c7b47c60e178dc
Joe Summerhayes <joesummerhayes09@hotmail.com>
2021-07-19 21:02:52 +0100

    display images and review main

--- app-client/src/views/Review.tsx
+++ app-client/src/views/Review.tsx
@@ -6,6 +6,8 @@ import { SliderComponent } from '../components/Slider';
 // import {colors, Typography} from '@material-ui/core';
 import { colors } from '../theme';
 import { RouteComponentProps } from 'react-router-dom';
+import getReview from '../data/get-review';
+import GVType from '../../../@types';
 
 const styles = makeStyles({
   image: {
@@ -19,29 +21,34 @@ const styles = makeStyles({
 type TParams = { id: string };
 
 export const Review: React.FC<RouteComponentProps<TParams>> = ({ match, location }) => {
-  console.log('11111', match, location);
+  const [ review, setReview ] = React.useState<GVType.Review>();
   const { params: { id } } = match;
   
   useEffect(() => {
     // use ID to get review data from graphQl
-  });
+    async function fetchReview(id: string) {
+      const review = await getReview({id});
+      setReview(review);
+    };
+    fetchReview(id);
+  }, []);
 
   const classes = styles();
-  return (
+  console.log(review);
+  return review ? (
     <>
     <div className="flex flex-column items-center">
-      <Title title={game1.title} bold size="large" className={classes.title} />
+      <Title title={review.title} bold size="large" className={classes.title} />
         <p className="mb5">
-          {game1.synopsis}
+          {review.synopsis}
         </p>
     </div>
-    <SliderComponent />
-    <div className="flex flex-column items-center">
+    <SliderComponent images={review.images} />
+    <div className="flex flex-column items-center lf-copy">
       <div className="f3 mt5">
-        {game1.body}
+        {review.review}
       </div>
     </div>
     </>
-    
-  )
+  ) : null
 };
